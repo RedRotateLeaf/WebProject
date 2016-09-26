1f167104e6a3f7bb9f9e56d59186a4124746eae0
USER-20140919IP\Administrator <2280333506@qq.com>
2016-09-26 21:11:10 +0800

    try first

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+function Paint(n:number):void {
+    for (var i = 0; i < n; i++) {
+        var front = "";
+        var kong = " ";
+        for (var j = 0; j < n - 1 - i; j++) {
+            front = front + kong;
+        }
+        var star = "*";
+        var starx = "";
+        for (var k = 0; k < 2 * i + 1; k++) {
+            starx = starx + star;
+        }
+        console.log(front + starx);
+    }
+}
+window.onload = function() {
+    Paint(8);
+};
